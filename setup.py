a95f31cdef612a9ba9efe9c5906339bcdcd467b6
jtoy <jtoy@jtoy.net>
2017-12-02 16:58:47 -0800

    preparing package

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-from setuptools import setup
- 
-setup(
-  name='sensenet',
-  version='0.1',
-  scripts=['env.py'],
-  author='Jason Toy',
-  url='http://sensenet.ai',
-  install_requires=[ 'numpy>=1.10.4', 'pybullet' ]
+from setuptools import setup, find_packages
+import sys, os.path
+
+# Don't import gym module here, since deps may not be installed
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'sensenet'))
+from version import VERSION
+
+setup(name='sensenet',
+      version=VERSION,
+      description='SenseNet',
+      url='https://github.com/jtoy/sensenet',
+      author='Jason Toy',
+      author_email='support@sensenet.ai',
+      license='',
+      packages=[package for package in find_packages()
+                if package.startswith('sensenet')],
+      zip_safe=False,
+      install_requires=[
+          'numpy>=1.10.4', 'requests>=2.0', 'six', 'pybullet'
+      ],
+      package_data={'sensenet': ['data/*']},
+      tests_require=['pytest'],
 )
