1a13e7035df8184c2fb024036958f6ae6851f860
jtoy <jtoy@jtoy.net>
2018-01-03 09:04:54 -0800

    touch classifier

--- scripts/generate_touches.py
+++ scripts/generate_touches.py
@@ -1,20 +1,8 @@
-import sys
+import sys,random,glob,argparse,uuid
 sys.path.append('..')
 import sensenet
-import random
 import pybullet as p
-
-if len(sys.argv) > 1:
-  envid = sys.argv[1]
-else:
-  envid = "HandEnv-v0"
-if len(sys.argv) > 2:
-  obj_path = sys.argv[2]
-else:
-  obj_path = None
-#TODO add support for folders,classes, and saving data
-env = sensenet.make(envid,{'render':True,'obj_path':obj_path})
-env.reset()
+import numpy as np
 
 forward = 3
 left = 0
@@ -25,100 +13,114 @@ indexup = 6
 indexdown = 7
 action_choices = [forward,left,right,up,down] #dont move index for now
 
-training_sets = []
-observations = []
-actions = []
-touch_count = 0
-step = 0
-episode = 0
-plan_step = 0
-winners = 0
 plans = [([left]*5),([right] * 5),([up] * 5),([down] * 5),([indexup] * 5),([indexdown] * 5),] + (["random"] * 6)
 def random_plan():
   a = []
   for i in range(5):
     a.append(random.randint(0,7))
-  return a      
+  return a
 plan = None
-for ii in range(p.getNumJoints(env.agent)):
-  print("info",p.getJointInfo(env.agent,ii))
+def save_data(env,label,touches,actions):
+  path = "touch_data/"+str(label)+"/"+str(uuid.uuid1())
+  env.mkdir_p(path)
+  np.save(path+"/touches",touches)
+  np.save(path+"/actions",actions)
 
-while (1):
-   points = p.getClosestPoints(env.obj_to_classify,env.agent,10000000,-1,22)
-   al = points[0][7]
-   ol = points[0][6]
-   #al, _ = p.getBasePositionAndOrientation(env.agent)
-   #ol, _ = p.getBasePositionAndOrientation(env.obj_to_classify)
-   xd = (al[0]-ol[0])/2
-   yd = (al[1]-ol[1])/2
-   zd = (al[2]-ol[2])/2
-   #print("xd",xd,"yd",yd,"zd",zd)
-   #what the hell is 22?
+#TODO add support for folders,classes, and saving data
+parser = argparse.ArgumentParser()
+parser.add_argument('--render', action='store_true', help='render the environment')
+parser.add_argument('--environment', type=str, default="HandEnv-v0")
+parser.add_argument('--epochs', type=int, default=1)
+parser.add_argument('--folder', type=str)
+parser.add_argument('--fast_exit', type=int, default=5)
+args = parser.parse_args()
+for filename in glob.iglob(args.folder+"/**/*.obj", recursive=True):
+  label = int(filename.split("/")[-3].split("_")[0])
+  print(filename)
+  print(label)
+  path = "touch_data/"+str(label)+"/"
+  env = sensenet.make(args.environment,{'render':args.render,'obj_path':filename})
+  env.mkdir_p(path)
+  training_sets = []
+  observations = []
+  actions = []
+  touch_count = 0
+  step = 0
+  episode = 0
+  plan_step = 0
+  winners = 0
+  for epoch in range(args.epochs):
+    env.reset()
+    while(1):
+      points = p.getClosestPoints(env.obj_to_classify,env.agent,10000000,-1,22)
+      al = points[0][7]
+      ol = points[0][6]
+      #al, _ = p.getBasePositionAndOrientation(env.agent)
+      #ol, _ = p.getBasePositionAndOrientation(env.obj_to_classify)
+      xd = (al[0]-ol[0])/2
+      yd = (al[1]-ol[1])/2
+      zd = (al[2]-ol[2])/2
+      #print("xd",xd,"yd",yd,"zd",zd)
+      #what the hell is 22?
 
-   if env.is_touching() and plan_step == 0:
-     plan = random.choice(plans)
-     if plan == "random":
-       plan = random_plan()
-     action = plan[0]
-     plan_step +=1
-   elif plan_step >0:
-     if len(plan) >= plan_step:
-       #reset to a new plan
-       plan_step = 0
-       plan = random.choice(plans)
-       if plan == "random":
-         plan = random_plan()
-     action = plan[plan_step]
-     plan_step += 1
-   elif not env.is_touching():
-     if random.random() > 0.7 and zd >= xd:
-     #elif random.random() > 0.3 and zd >= xd and zd >= yd:
-       #print('z')
-       action = down
-     elif random.random() > 0.4 and xd >= yd:
-     #if random.random() > 0.3 and xd >= yd and xd >= zd:
-       #print('x')
-       #action = forward
-       action = left
-     elif random.random() > 0.3 and yd >= xd:
-     #elif random.random() > 0.3 and yd >= xd and yd >= zd:
-       #print('y')
-       #action = left
-       action = forward
-     else:
-       #action = forward #should never get here
-       action = random.choice(action_choices)
+      if env.is_touching() and plan_step == 0:
+        plan = random.choice(plans)
+        if plan == "random":
+          plan = random_plan()
+        action = plan[0]
+        plan_step +=1
+      elif plan_step >0:
+        if len(plan) >= plan_step:
+          #reset to a new plan
+          plan_step = 0
+          plan = random.choice(plans)
+          if plan == "random":
+            plan = random_plan()
+        action = plan[plan_step]
+        plan_step += 1
+      elif not env.is_touching():
+        if random.random() > 0.7 and zd >= xd:
+          action = down
+        elif random.random() > 0.4 and xd >= yd:
+          action = left
+        elif random.random() > 0.3 and yd >= xd:
+          action = forward
+        else:
+          action = random.choice(action_choices)
 
-   observation,reward,done,info = env.step(action)
+      observation,reward,done,info = env.step(action)
 
-   if env.is_touching():
-     observations.append(observation)
-     actions.append(action)
-     touch_count += 1
-     print("is touching")
-   if touch_count >= 20:
-     print("WINNER!!")
-     winners += 1
-     touch_count = 0
-     training_sets.append([observations,actions])
-     observations = []
-     actions = []
-     step = 0
-     episode +=1
-     plan_step = 0
-     env.reset()
-   elif step >= 100:
-     print("closing episode,touch_count",touch_count)
-     touch_count = 0
-     if len(observations) > 1:
-       training_sets.append([observations,actions])
-     observations = []
-     actions = []
-     step = 0
-     episode +=1
-     plan_step = 0
-     env.reset()
-   if step % 5 == 0:
-     print("episode",episode,"step", step,"plan", plan,"plan_step",plan_step,"touch_count",touch_count,"winners",winners)
-   step +=1 
-
+      if env.is_touching():
+        observations.append(observation.reshape(200,200))
+        actions.append(action)
+        touch_count += 1
+        #print("is touching")
+      if touch_count >= 20:
+        print("WINNER!!")
+        winners += 1
+        touch_count = 0
+        #training_sets.append([observations,actions])
+        save_data(env,label,observations,actions)
+        observations = []
+        actions = []
+        step = 0
+        episode +=1
+        plan_step = 0
+        env.reset()
+      elif step >= 100:
+        print("closing episode,touch_count",touch_count)
+        touch_count = 0
+        if len(observations) > 2:
+          #training_sets.append([observations,actions])
+          save_data(env,label,observations,actions)
+        observations = []
+        actions = []
+        step = 0
+        episode +=1
+        plan_step = 0
+        env.reset()
+      if args.fast_exit != 0 and episode >= args.fast_exit:
+          sys.exit()
+      if step % 10 == 0:
+        print("episode",episode,"step", step,"plan", plan,"plan_step",plan_step,"touch_count",touch_count,"winners",winners)
+      step +=1
