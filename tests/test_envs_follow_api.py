2be65623187bab35347c738ecfd595a533cade21
Jason Toy <jtoy@jtoy.net>
2018-02-17 18:40:17 -0800

    env api tests

---
+++ tests/test_envs_follow_api.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append('..')
+import sensenet
+from sensenet import envs
+from contextlib import contextmanager
+
+def test_envs_follow_api():
+    env_names = [spec.id for spec in envs.registry.all()]
+    for name in env_names:
+        print("loading env",name)
+        env = sensenet.make(name)
+        assert env.action_space.n
+        reset_observation = env.reset()
+        assert(reset_observation is not None)
+        observation = env.step(0)
+        assert observation != None
+
+        #put any methods in here to test exceptions
+        try:
+            #end.seed(42)
+            env.reset()
+            env.render()
+            assert True
+        except:
+            raise Exception('spam', 'eggs')
+
